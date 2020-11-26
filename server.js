17ba85db8a62833964ce176e92120c608a4b8fea
Alba2020 <alba2020@gmail.com>
2020-11-26 11:35:25 -0800

    add signals

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const HTTP_PORT = 8000
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.listen(HTTP_PORT, () => {
+const server = app.listen(HTTP_PORT, () => {
   console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
 })
 
@@ -46,3 +46,28 @@ app.delete("/api/orders/:id", ordersCtrl.delete)
 app.use(function (req, res, next) {
   res.status(404).send("Sorry can't find that!")
 })
+
+
+// The signals we want to handle
+// NOTE: although it is tempting, the SIGKILL signal (9) cannot be intercepted and handled
+var signals = {
+  'SIGHUP': 1,
+  'SIGINT': 2,
+  'SIGTERM': 15
+};
+// Do any necessary shutdown logic for our application here
+const shutdown = (signal, value) => {
+  console.log("shutdown!");
+  server.close(() => {
+    console.log(`server stopped by ${signal} with value ${value}`);
+    process.exit(128 + value);
+  });
+};
+
+// Create a listener for each of the signals that we want to handle
+Object.keys(signals).forEach((signal) => {
+  process.on(signal, () => {
+    console.log(`process received a ${signal} signal`);
+    shutdown(signal, signals[signal]);
+  });
+});
